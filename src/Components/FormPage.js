6995f7276eb10d789747f9314836af2b25a757e4
Tusar <tusarahmed141@gmail.com>
2022-10-22 20:37:39 +0600

    Mobile part complete

--- src/Components/FormPage.js
+++ src/Components/FormPage.js
@@ -3,19 +3,19 @@ import FormValidation from './FormValidation';
 
 const FormPage = () => {
   return (
-    <section className='section'>
-      <div className="container">
+    <main className='main-section'>
+      <section className="container">
         <div className='form-description'>
-          <h2 className="description__heading">Learn to code by watching other</h2>
+          <h2 className="description__heading">Learn to code by watching others</h2>
           <p className="description__para">See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understating how developers think is invaluable.</p>
         </div>
-        <div className='form-container'>
-          <span className='form__alert'>Try it free 7 days then $20/mo. thereafter</span>
 
+        <div className='form-container'>
+          <p className='form__alert'><span className='font-bold'>Try it free 7 days</span> then $20/mo. thereafter</p>
           <FormValidation />
         </div>
-      </div>
-    </section>
+      </section>
+    </main>
   );
 };
 
