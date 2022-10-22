66704cb1c83abc641921d02b0534c9c9f030854a
Tusar <tusarahmed141@gmail.com>
2022-10-22 19:21:21 +0600

    Form validation markup done

--- src/Components/FormValidation.js
+++ src/Components/FormValidation.js
@@ -1,11 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class FormValidation extends Component {
   render() {
     return (
-      <div>FormValidation</div>
-    )
+      <div className="form">
+        <form>
+          <div className="form__input-group">
+            <input type="text" name="first-name" className="form__input" />
+          </div>
+          <div className="form__input-group">
+            <input type="text" name="last-name" className="form__input" />
+          </div>
+          <div className="form__input-group">
+            <input type="email" name="email" className="form__input" />
+          </div>
+          <div className="form__input-group">
+            <input type="password" name="password" className="form__input" />
+          </div>
+
+          <button className="form__btn">Claim your free trial</button>
+          <span className="form__terms">By clicking the button, you are agreeing to our Terms and Services</span>
+        </form>
+      </div>
+    );
   }
 }
 
-export default FormValidation
+export default FormValidation;
