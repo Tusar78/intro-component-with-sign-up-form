7a44b614c985ab76e4f4899573574e7488469294
Tusar <tusarahmed141@gmail.com>
2022-10-22 19:09:30 +0600

    Markup almost done

--- src/Components/FormPage.js
+++ src/Components/FormPage.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import FormValidation from './FormValidation';
 
 const FormPage = () => {
   return (
-    <div >
-      
-    </div>
+    <section className='section'>
+      <div className="container">
+        <div className='form-description'>
+          <h2 className="description__heading">Learn to code by watching other</h2>
+          <p className="description__para">See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understating how developers think is invaluable.</p>
+        </div>
+        <div className='form-container'>
+          <span className='form__alert'>Try it free 7 days then $20/mo. thereafter</span>
+
+          <FormValidation />
+        </div>
+      </div>
+    </section>
   );
 };
 
